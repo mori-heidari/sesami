b68c2f9f16f29c5f6611992085c157d1c2bdb83b
mohammad <mm4heidary@gmail.com>
2022-12-03 14:55:59 +0330

    update appoinment service  to save state

--- src/services/appointment/appointment.service.js
+++ src/services/appointment/appointment.service.js
@@ -1,25 +1,36 @@
 const config = require('config');
 const AppError = require('./../error/error.class')
-const {Appointment} = require("./../../models/appointment.model");
+const {Appointment, AppointmentState} = require("./../../models/appointment.model");
 const create = async (id, start, end) => {
     return new Promise(async (resolve, reject) => {
         try {
             const checkDate = await Appointment.findOne({
-                $and:[
-                    {$or:[
-                            {$and: [{start: {$lte: start}},{end: {$gte: start}}]},
-                            {$and: [{start: {$lte: end}},{end: {$gte: end}}]},
-                        ]},
-                    {id:{$ne:id}}
+                $and: [
+                    {
+                        $or: [
+                            {$and: [{start: {$lte: start}}, {end: {$gte: start}}]},
+                            {$and: [{start: {$lte: end}}, {end: {$gte: end}}]},
+                        ]
+                    },
+                    {id: {$ne: id}}
                 ]
             });
             if (checkDate) throw new AppError("this time range is reserved!");
+            await AppointmentState.create({
+                id: id,
+                start: start,
+                end: end,
+
+            });
             const appointment = await Appointment.update({
                 id: id,
-            },{
-                start:start,
-                end:end,
-            },{upsert: true, setDefaultsOnInsert: true});
+            }, {
+                start: start,
+                end: end,
+
+            }, {upsert: true, setDefaultsOnInsert: true});
+            //appointment.historical_data.push({start,end});
+            //await appointment.save();
             resolve(appointment);
         } catch (error) {
             return reject(error);
@@ -31,8 +42,8 @@ const getByRange = async (start, end) => {
     return new Promise(async (resolve, reject) => {
         try {
             const appointment = await Appointment.find({
-                start: {$gte:start},
-                end: {$lte:end},
+                start: {$gte: start},
+                end: {$lte: end},
             });
             resolve(appointment);
         } catch (error) {
